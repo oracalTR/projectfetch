ed1508924982a4c371115e110185b6e1c65a8cc6
Pavel Trushnikov <oracal@mail.ru>
2022-01-06 23:23:22 +0300

    Выполнение задания 26 урока метод Fetch

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ const getData = (url) => {
         .then(response => response.json())
         .then(data => {
             console.log('Данные из  файла', data);
-            const sendData = (url, data) => {
-                return fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json;charset=utf-8'
-                    },
-                    body: JSON.stringify(data)
-                })
-                    .then(response => response.json())
-                    .then(data => console.log('Данные отправленные на сервер', data))
-                    .catch('Ошибка отправки данных');
-            };
-            sendData('https://jsonplaceholder.typicode.com/posts', data);
+            sendData('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => response.json())
+            .then(data => console.log('Данные отправленные на сервер', data))
+            .catch(error => {
+                console.log('Ошибка отправки данных');
+            });
         })
-        .catch('Ошибка получения данных');
+        .catch(erorr => console.log('Ошибка получения данных'));
+};
+
+const sendData = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+    });
 };
 
 getData('./db.json');
